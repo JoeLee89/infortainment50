39be7fed62ff6700b430dd469f98d75c2300a32c
palsmihe <palsmihe@gmail.com>
2022-12-20 14:22:08 +0800

    add extra test items in other function which can't get lauched in loop reading from auto batch file.

--- overnight.sh
+++ overnight.sh
@@ -4,7 +4,8 @@ times=0
 
 loop_time=$(date +%s --date="+12 hour")
 file_name="all_tests_auto_EBOARD_ADi_$board.sh"
-
+#file_name="test.txt"
+#sed -i 's/"/\\"/g' $file_name
 
 #set start time will make the program pause, until the setting time reach
 #input start time format ex. "10/26/21-11" to start setting wait time, or set to 0 to skip the function
@@ -13,17 +14,45 @@ start_time=0
 
 #to set others=1 will set the other() function launch, if your project support the script
 #or just set to =0 to skip the test
-others_script=0
+others_script=1
 
 other() {
-  print_command "sudo ./idll-test"$executable" -- --EBOARD_TYPE EBOARD_ADi_"$board" "Scenario: adiWatchdogSetSystemRestart" -s"
+#  print_command "sudo ./idll-test"$executable" -- --EBOARD_TYPE EBOARD_ADi_"$board" "Scenario: adiWatchdogSetSystemRestart" -s"
 
-  other=$(sudo ./idll-test"$executable" -- --EBOARD_TYPE EBOARD_ADi_"$board" "Scenario: adiWatchdogSetSystemRestart" -s)
-  echo "$other"
-  echo "================================================================================================" >> result.log
-  echo "sudo ./idll-test"$executable" -- --EBOARD_TYPE EBOARD_ADi_"$board" "Scenario: adiWatchdogSetSystemRestart" -s" >> result.log
-  echo "================================================================================================" >> result.log
-  echo "$other" >> result.log
+  other_00=$(./idll-test"$executable" -- --EBOARD_TYPE EBOARD_ADi_"$board" "Scenario: adiWatchdogSetSystemRestart")
+  other_00_cmd="./idll-test"$executable" -- --EBOARD_TYPE EBOARD_ADi_"$board" \"Scenario: adiWatchdogSetSystemRestart\""
+  other_01=$(./idll-test"$executable" -- --EBOARD_TYPE EBOARD_ADi_"$board" "Scenario: adiBatSetLowVoltage")
+  other_01_cmd="./idll-test"$executable" -- --EBOARD_TYPE EBOARD_ADi_"$board" \"Scenario: adiBatSetLowVoltage\""
+  other_02=$(./idll-test"$executable" --BAT-QTY 3 -- --EBOARD_TYPE EBOARD_ADi_"$board" "Scenario: adiBatSetWarningVoltage")
+  other_02_cmd="./idll-test"$executable" --BAT-QTY 3 -- --EBOARD_TYPE EBOARD_ADi_"$board" \"Scenario: adiBatSetWarningVoltage\""
+
+  for i in $(seq 0 2); do
+    case $i in
+    0)
+      other=$other_00
+      other_cmd=$other_00_cmd
+
+      ;;
+    1)
+      other=$other_01
+      other_cmd=$other_01_cmd
+
+      ;;
+    2)
+      other=$other_02
+      other_cmd=$other_02_cmd
+
+      ;;
+    esac
+
+    print_command "$other_cmd"
+    echo "$other"
+    echo "================================================================================================" >> result.log
+    echo "$other_cmd" >> result.log
+    echo "================================================================================================" >> result.log
+    compare_result "$other" "passed"
+    echo "$other" >> result.log
+  done
 }
 
 wait_times() {
@@ -54,10 +83,11 @@ while true; do
   fi
 
   while read line; do
-    con=$(echo "$line" | grep -i "idll-test" | grep -v "#" | sed "s/\r//g")
+    con=$(echo "$line" | grep -i "idll-test" | grep -v "#\|\"" | sed "s/\r//g")
 
     if [[ "${#con}" -ne 0 ]]; then
       for (( i = 1; i < $(shuf -i 3-6 -n 1); i++ )); do
+
           echo "$(date +%D-%T)" >> result.log
           launch_command "$con"
           echo "================================================================================================" >> result.log
